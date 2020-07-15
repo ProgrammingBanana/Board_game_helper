13665a8959af9aa11a5d21daf44a8586e190bd11
ProgrammingBanana <daniel.jesus@upr.edu>
2020-07-15 15:32:10 -0400

    separated the lookup input command from the lookup video_lookup.py file

---
+++ youtube-search/app.py
@@ -0,0 +1,5 @@
+from video_lookup import YouTubeInfo
+
+
+look_up = input("What would you like to look up?")
+youtube_search = YouTubeInfo(look_up)
--- youtube-search/video_lookup.py
+++ youtube-search/video_lookup.py
@@ -2,50 +2,76 @@ import requests
 import settings
 import json
 
+class YouTubeInfo:
+    SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
 
-SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
+    def __init__(self, lookup_string):
+        self.lookup_string = lookup_string
+        self.youtube_search(self.lookup_string)
 
-# class YouTubeInfo:
+    def new_lookup_string(lookup_string):
+        """
+        new_lookup_string methd to change the lookup string.
 
-#     def __init__(self, lookup_string):
-#         self.lookup_string = lookup_string
+        Args:
+            lookup_string (str): new search string in case the user wants to change their search
+        """
+        self.lookup_string = lookup_string
+        self.youtube_search(self.lookup_string)
 
-def youtube_lookup_results(lookup_string):
-    
-    search_params = {
-    'part' : 'snippet',
-    'q' : lookup_string,
-    'maxResults' : 10,
-    'type' : 'video',
-    'order' : 'relevance',
-    'key' : settings.YOUTUBE_DATA_API_KEY
-    }
+    def youtube_search(self, lookup_string):
+        """
+        youtube_search This uses the YouTube Data API to look for the top videos results
+                       for a given string
 
-    r = requests.get(SEARCH_URL, params = search_params)
-    results = r.json()['items']
-    video_id_list = []
+        Args:
+            lookup_string (str): What the user wants to search youtube for
 
-    for result in results: 
-        video_id_list.append(result['id']['videoId'])
+        Returns:
+            list : a list of the top videos for a given search. 
+        """
 
-    main_watch_url = "http://www.youtube.com/watch?v="
+        search_params = {
+        'part' : 'snippet',
+        'q' : lookup_string,
+        'maxResults' : 10,
+        'type' : 'video',
+        'order' : 'relevance',
+        'key' : settings.YOUTUBE_DATA_API_KEY
+        }
 
-    video_links = []
-    for id in video_id_list:
-        video_links.append(main_watch_url +id)
- 
-    return video_links
+        r = requests.get(self.SEARCH_URL, params = search_params)
+        results = r.json()['items']
+        video_id_list = []
+
+        for result in results: 
+            video_id_list.append(result['id']['videoId'])
+
+        main_watch_url = "http://www.youtube.com/watch?v="
+
+        video_links = []
+        for id in video_id_list:
+            video_links.append(main_watch_url +id)
+
+        self.generate_video_link_file(video_links)
+
+        return video_links
+
+    def generate_video_link_file(self, top_videos):
+        """
+        generate_video_link_file This generates a file named video-links.txt Where we store
+                                 the links for the latest YouTube search
+
+        Args:
+            top_videos (list): a list of the top YouTube search results for the given lookup string
+        """ 
+        with open("video-links.txt", "w") as videos_links:
+            for video in top_videos:
+                videos_links.write(video +"\n")
+        return 
 
 
-def generate_video_link_file(top_videos):
-    with open("video-links.txt", "w") as videos_links:
-        for video in top_videos:
-            videos_links.write(video +"\n")
-    return 
 
 
-look_up = input("What would you like to look up?")
-top_ten_videos = youtube_lookup_results(look_up)
-generate_video_link_file(top_ten_videos)
 
 
